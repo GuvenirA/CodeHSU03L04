168b12e5dfe01e1145238e4c3ab1de798caad69e
GuvenirA <guvenira@wlhs.wlwv.k12.or.us>
2022-11-08 09:03:39 -0800

    partially done

--- src/BillTester.java
+++ src/BillTester.java
@@ -20,10 +20,12 @@ public class BillTester
         System.out.println("\n\n"); // print a few blank lines
         // Another dinner with 5 guests
         System.out.println("Birthday Dinner");
-        System.out.println();
+        System.out.println(birthdayDinner);
 
         // Add the tip
         // Notice the difference in the final amount
+        birthdayDinner.addTip();
+        System.out.println("\n");
 
         System.out.println("\nBirthday Dinner after tip");
         System.out.println();
